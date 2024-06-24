a9f3e06b99a9dd48e324c5308156ab92fe28dc3c
Elisha-Kiplangat <elishakiplanga@gmail.com>
2024-06-24 23:11:32 +0300

    commentreducer

--- src/components/CommentReducer.tsx
+++ src/components/CommentReducer.tsx
@@ -1,16 +1,38 @@
+export interface Comment {
+    id: number;
+    user: string;
+    time: string;
+    content: string;
+    likes: number;
+
+}
+
+type Action = 
+  { type: 'ADD_COMMENT', payload: Comment } 
+| { type: 'LIKE_COMMENT', payload: number }
 
 
 const CommentReducer = () => {
-  return (
-    <>
-
-    </>
-  )
+    return (state: Comment[], action: Action): Comment[] => {
+        switch(action.type){
+            case 'ADD_COMMENT':
+                return [...state, action.payload]
+            case 'LIKE_COMMENT':
+                return state.map((comment) => 
+                    comment.id === action.payload ? {...comment, likes: comment.likes + 1}
+                    : comment
+                )
+            default:
+                return state
+        }
+    }
+  
+  
 }
 
 export default CommentReducer
 
-export const initialComments = [
+export const initialComments: Comment[] = [
         {
             id: 1,
             user: 'John Doe',
